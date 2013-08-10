e197c8b580b44d2f07ce7b994e6474165b02531e
telega1 <telega@gmail.com>
2013-08-10 10:30:36 +0200

    Update version

--- chrome/content/legacy.js
+++ chrome/content/legacy.js
@@ -1,7 +1,7 @@
 var FdLegacy = new function() {
     this.migrate = function() {
         var previousVersion = FdPrefs.getString("version");
-        var version = "4.10";
+        var version = "4.11";
         FdPrefs.setString("version", version);
 
         if (!previousVersion) addToolbarButton();
