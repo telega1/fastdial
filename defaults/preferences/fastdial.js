25c5b592e4db10f697cb490fd7f2d4f2e9a98248
telega1 <telega@gmail.com>
2016-01-26 17:46:06 +0300

    Set "view_source.tab" to false
    To prevent opening page source in a tab, which erroneously pops up the
    Fast Dial page.

--- defaults/preferences/fastdial.js
+++ defaults/preferences/fastdial.js
@@ -2,3 +2,4 @@ pref("extensions.fastdial@telega.phpnet.us.description", "chrome://fastdial/loca
 pref("extensions.fastdial.slowSites", "gmail.com|mail\\.google|google\\.com\\/mail|reader\\.google|google\\.com\\/reader");
 pref("extensions.fastdial.options", "{ \"width\": 3, \"height\": 3, \"thumbWidth\": 300, \"timeout\": 1000 }");
 pref("extensions.fastdial.enable", true);
+pref("view_source.tab", false);
