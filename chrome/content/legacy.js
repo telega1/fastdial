64140a902fb5c63a9a7d891206ff4cceb532a1dc
telega1 <telega@gmail.com>
2016-04-07 22:04:03 +0300

    Update version

--- chrome/content/legacy.js
+++ chrome/content/legacy.js
@@ -1,7 +1,7 @@
 fastdial.Legacy = new function() {
     this.migrate = function() {
         var previousVersion = fastdial.Prefs.getString("version");
-        var version = "4.14.4";
+        var version = "4.14.5";
         fastdial.Prefs.setString("version", version);
         if (version != previousVersion) 
                                 initThemes();
